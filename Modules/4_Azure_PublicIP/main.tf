f49c446bd23c0387918de9bc5f923a1115927f2f
shubhamsinghvi8696 <sshubham8696@gmail.com>
2024-11-23 23:11:38 +0530

    Add Public IP Variables

--- Modules/4_Azure_PublicIP/main.tf
+++ Modules/4_Azure_PublicIP/main.tf
@@ -1,11 +1,4 @@
-variable "var_PiP" {
-  type = map(object({
-    name                = string
-    resource_group_name = string
-    location            = string
-  }))
 
-}
 resource "azurerm_public_ip" "pip" {
   for_each            = var.var_PiP
   name                = each.value.name
